21a5a28371f18795120143ac4d5a3920027ba402
JayGee <jayguarino@gmail.com>
2023-07-08 16:10:55 -0700

    working on svg animation

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import './index.scss';
 import { useEffect, useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
+import Logo from './Logo';
 
 const Home = () => {
     
@@ -40,6 +41,7 @@ const Home = () => {
             </h2>
             <Link to='/contact' className='flat-button'>CONTACT ME</Link>
         </div>
+        <Logo />
     </div>
   )
 }
