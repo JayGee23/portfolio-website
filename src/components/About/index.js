92500497beddc8e0a9283dca5ac2f6d532752fcb
JayGee <jayguarino@gmail.com>
2023-07-09 15:21:02 -0700

    pacman loader added

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,9 +3,9 @@ import AnimatedLetters from '../AnimatedLetters'
 import { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBootstrap, faCss3, faGitAlt, faHtml5, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons';
+import Loader from 'react-loaders';
 
 const About = () => {
-
     const [letterClass, setLetterClass] = useState('text-animate');
 
     useEffect(() => {
@@ -19,7 +19,8 @@ const About = () => {
     }, []);
 
     return (
-        <div className='container about-page'>
+        <>
+            <div className='container about-page'>
             <div className='text-zone'>
                 <h1>
                     <AnimatedLetters
@@ -61,7 +62,10 @@ const About = () => {
                     </div>
                 </div>
             </div>
-        </div>
+            </div>
+
+            <Loader type='pacman' />
+        </>
     )
 }
 
