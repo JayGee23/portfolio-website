e38ce3b3d68fd26cf407354ce55b0685c382a266
JayGee <jayguarino@gmail.com>
2023-07-15 12:38:03 -0700

    map added

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,6 +3,7 @@ import './index.scss';
 import AnimatedLetters from '../AnimatedLetters';
 import { useEffect, useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
+import { MapContainer, Marker, Popup, TileLayer} from 'react-leaflet';
 
 const Contact = () => {
     const [letterClass, setLetterClass] = useState('text-animate');
@@ -72,6 +73,25 @@ const Contact = () => {
                             </ul>
                         </form>
                     </div>
+                </div>
+                <div className='info-map'>
+                    Jay Guarino,
+                    <br />
+                    Vancouver, BC
+                    <br />
+                    Canada 
+                    <br />
+                    <span>jayguarino@gmail.com</span>
+                </div>
+                <div className='map-wrap'>
+                <MapContainer center={[49.271298360778495, -123.10632884888848]} zoom={16}>
+                    <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" 
+                    />
+                    <Marker position={[49.271298360778495, -123.10632884888848]}>
+                        <Popup className='popup'>Thanks for stopping by!</Popup>
+                    </Marker>
+                </MapContainer>
                 </div>
             </div>
 
