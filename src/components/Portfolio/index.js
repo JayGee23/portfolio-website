97f181232419145095f4743b28a05443e9846fe9
JayGee <jayguarino@gmail.com>
2023-07-16 13:15:04 -0700

    added portfolio project template, still needs styling

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,8 +1,62 @@
+import Loader from 'react-loaders';
+import './index.scss';
+import AnimatedLetters from '../AnimatedLetters';
+import { useState, useEffect } from 'react';
+import portfolioData from '../../data/portfolio.json';
+
 const Portfolio = () => {
+
+    const [letterClass, setLetterClass] = useState('text-animate');
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+          setLetterClass('text-animate-hover');
+        }, 3000);
+      
+        return () => {
+          clearTimeout(timeoutId);
+        };
+    }, []);
     
-  return (
-    <div>Portfolio</div>
-  )
+    const renderPortfolio = (portfolio) => {
+        return (
+            <div className='images-container'>
+                {
+                    portfolio.map((port, idx) => {
+                        return (
+                            <div className='image-box' key={idx}>
+                                <img 
+                                    src={port.cover}
+                                    className='portfolio-image'
+                                    alt='project' 
+                                />
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        )
+    }
+
+    return (
+        <>
+            <div className='container portfolio-page'>
+                <h1 className='page-title'>
+                    <AnimatedLetters 
+                        letterClass={letterClass}
+                        strArray={[...'Portfolio']}
+                        idx={15}
+                    
+                    />
+                </h1>
+                <div>
+                    {renderPortfolio(portfolioData.portfolio)}
+                </div>
+            </div>
+
+            <Loader type='pacman'/>
+        </>
+    )
 }
 
-export default Portfolio
+export default Portfolio;
