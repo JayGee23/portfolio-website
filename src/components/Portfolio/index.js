141fc3e8ace27bc255610ffae7005fd8bfafc2f1
JayGee <jayguarino@gmail.com>
2023-07-17 18:28:32 -0700

    portfolio projects styling in progress

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -30,6 +30,11 @@ const Portfolio = () => {
                                     className='portfolio-image'
                                     alt='project' 
                                 />
+                                <div className='content'>
+                                    <p className='title'>{port.title}</p>
+                                    <h4 className='description'>{port.description}</h4>
+                                    <button className='button' onClick={() => window.open(port.url)}>View</button>
+                                </div>
                             </div>
                         )
                     })
